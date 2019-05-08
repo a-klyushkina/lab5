7b614aa38aee9caddd5dff8edb6fc1fe542eec06
a-klyushkina <a-klyushkina@mail.ru>
2019-05-08 11:58:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var createError = require("http-errors");
+var express = require("express");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+require("./database");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+module.exports = app;
